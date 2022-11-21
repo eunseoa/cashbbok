c93f66a6cb5c31f58fad9398998ae6e522da52a2
LG <LG@DESKTOP-R5N449P>
2022-11-21 10:11:55 +0900

    vo 생성(도메인타입)

---
+++ src/main/java/vo/Cash.java
@@ -0,0 +1,51 @@
+package vo;
+
+public class Cash {
+	// 정보은닉
+	private int cashNo;
+	private int categoryNo; // 외래키가 있으면 INNER JOIN 발생할 수 있음 (Map타입으로 해결)
+	private long cashPrize;
+	private String cashMemo;
+	private String updatedate;
+	private String createdate;
+	// 캡슐화
+	public int getCashNo() {
+		return cashNo;
+	}
+	public void setCashNo(int cashNo) {
+		this.cashNo = cashNo;
+	}
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public long getCashPrize() {
+		return cashPrize;
+	}
+	public void setCashPrize(long cashPrize) {
+		this.cashPrize = cashPrize;
+	}
+	public String getCashMemo() {
+		return cashMemo;
+	}
+	public void setCashMemo(String cashMemo) {
+		this.cashMemo = cashMemo;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	
+	
+	
+}
